41fc3453e2230acaa9410d1dc339a0ae8c0cb6b6
Will Smidlein <Will@WillSmidlein.com>
2013-10-19 00:35:45 -0400

    Super duper basic unit testing setup

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,13 @@
-var assert = require('assert')
+var should = require('chai').should()
+
 var path = require('path')
 var rootDir = path.join(__dirname, '..')
 
+var imageHelper = require(rootDir + '/lib/imageHelper.js')
+describe("Image Helper Tests", function() {
+
+    it("should be a function", function(){
+    	imageHelper.should.be.a('function')
+    })
+
+})
