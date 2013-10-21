fa9bc69e1961b8fa3171ef4a3f1be69c91b9dab6
Charley Hutchison <glenwayguy@gmail.com>
2013-10-20 19:30:02 -0500

    Fixed error in colorChange.js

--- colorChange.js
+++ colorChange.js
@@ -4,9 +4,30 @@ var config = require('./config.json')
 var imageHelper = require('./lib/imageHelper.js')
 var randomColor = require('./lib/randomColor.js')
 var fs = require('fs')
-var schedule = require('node-schedule');
+var schedule = require('node-schedule')
 
-var rule = new schedule.RecurrenceRule();
+var changeBackgroundColor = function(user) {
+	var twitterAPI = new Twit({
+		consumer_key: config.twitter.oauth_token,
+		consumer_secret: config.twitter.oauth_secret,
+		access_token: user.token,
+		access_token_secret: user.secret
+	})
+	randomColor(function(err, color) {
+		imageHelper.genImage(user.path, color, function(err, coloredImage) {
+			twitterAPI.post('account/update_profile_image', { image: coloredImage.toString('base64')}, function(err, reply) {
+				if(err){
+					if(err.code === 89 || err.message === 'Invalid or expired token') {
+						connection.query('DELETE FROM users WHERE id = ' + user.id, function(err) {
+						})
+					}
+				}
+			})
+		})
+	})
+}
+
+var rule = new schedule.RecurrenceRule()
 rule.hour = 0
 rule.minute = 0
 
@@ -14,26 +35,4 @@ var colorChange = schedule.scheduleJob(rule, function(){
 	connection.query("SELECT * FROM users WHERE path IS NOT NULL", function(err, rows) {
 		rows.forEach(changeBackgroundColor)
 	})
-}
-
-
-function changeBackgroundColor(user) {
-	var twitterAPI = new Twit({
-		consumer_key: config.twitter.oauth_token,
-		consumer_secret: config.twitter.oauth_secret,
-		access_token: user.token,
-		access_token_secret: user.secret
-	})
-		randomColor(function(err, color) {
-			imageHelper.genImage(user.path, color, function(err, coloredImage) {
-				twitterAPI.post('account/update_profile_image', { image: coloredImage.toString('base64')}, function(err, reply) {
-					if(err){
-						if(err.code === 89 || err.message === 'Invalid or expired token') {
-							connection.query('DELETE FROM users WHERE id = ' + user.id, function(err) {
-							})
-						}
-					}
-				})
-			})
-		})
-}
+});
